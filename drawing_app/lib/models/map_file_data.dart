e1fb1738df2ff17ba5f5201b802a192fabf71dd9
vivienphang <vivienphang@gmail.com>
2023-11-02 11:57:09 +0800

    updated map_file_data types

--- drawing_app/lib/models/map_file_data.dart
+++ drawing_app/lib/models/map_file_data.dart
@@ -21,6 +21,17 @@ class ImageData {
       'routes': routes,
     };
   }
+
+  factory ImageData.fromJson(Map<String, dynamic> json) {
+    return ImageData(
+      name: json['name'],
+      imageUrl: json['image_url'],
+      zones: (json['zones'] as List)
+          .map((zoneJson) => Zone.fromJson(zoneJson))
+          .toList(),
+      routes: json['routes'], // Assuming 'routes' is directly usable
+    );
+  }
 }
 
 class Zone {
@@ -35,6 +46,15 @@ class Zone {
       'Valid': valid,
     };
   }
+
+  factory Zone.fromJson(Map<String, dynamic> json) {
+    return Zone(
+      points: (json['P'] as List)
+          .map((pointJson) => Point.fromJson(pointJson))
+          .toList(),
+      valid: json['Valid'] ?? true,
+    );
+  }
 }
 
 class Point {
@@ -43,10 +63,19 @@ class Point {
 
   Point({required this.x, required this.y});
 
+  // toJson converts Dart object into JSON
   Map<String, dynamic> toJson() {
     return {
       'X': x,
       'Y': y,
     };
+  }
+
+  // fromJson creates instance of the class from JSON
+  factory Point.fromJson(Map<String, dynamic> json) {
+    return Point(
+      x: json['X'].toDouble(),
+      y: json['Y'].toDouble(),
+    );
   }
 }
