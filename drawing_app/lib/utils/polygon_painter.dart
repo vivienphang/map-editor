7ecc1b45e7df1c34daf40e0e0a3923ddcaa1d81c
vivienphang <vivienphang@gmail.com>
2023-11-03 16:55:48 +0800

    updated to include screenSize and transformationMatrix

--- drawing_app/lib/utils/polygon_painter.dart
+++ drawing_app/lib/utils/polygon_painter.dart
@@ -1,19 +1,53 @@
 import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
 export 'polygon_painter.dart';
 
 class PolygonPainter extends CustomPainter {
   final List<Offset?> points;
+  final Function(Offset)? onNewPointAdded;
+  final Size? screenSize;
 
-  PolygonPainter(this.points);
+  // NEW
+  final Matrix4 transformationMatrix;
+
+  PolygonPainter(
+      {required this.points,
+      this.onNewPointAdded,
+      this.screenSize,
+
+      //NEW
+      required this.transformationMatrix});
 
   @override
   void paint(Canvas canvas, Size size) {
-    if (points.isEmpty) return;
-
-    final linePaint = Paint()
+    // NEW Use the transformation matrix to apply scaling and translation
+    canvas.transform(transformationMatrix.storage);
+    final paint = Paint()
       ..color = Colors.red
       ..strokeCap = StrokeCap.round
       ..strokeWidth = 5.0;
+
+    // If points are empty and allowDrawing is true, just draw an empty canvas
+    if (points.isEmpty
+        //&& allowDrawing
+        ) {
+      // The canvas is ready for user drawing. Nothing else to render.
+      return;
+    }
+
+    // Existing points are not empty, scale them to the canvas size
+    final List<Offset> scaledPoints =
+        points.where((point) => point != null).map((point) {
+      final double scaleX = size.width / screenSize!.width;
+      final double scaleY = size.height / screenSize!.height;
+      return Offset(point!.dx * scaleX, point.dy * scaleY);
+    }).toList();
+
+    // Draw the polygon if points are not empty
+    if (scaledPoints.isNotEmpty) {
+      final path = Path()..addPolygon(scaledPoints, true);
+      canvas.drawPath(path, paint);
+    }
 
     final double squareSize = 10.0;
 
@@ -58,14 +92,14 @@ class PolygonPainter extends CustomPainter {
     // Draw lines between the points
     for (int i = 0; i < points.length - 1; i++) {
       if (points[i] != null && points[i + 1] != null) {
-        canvas.drawLine(points[i]!, points[i + 1]!, linePaint);
+        canvas.drawLine(points[i]!, points[i + 1]!, paint);
       }
     }
 
     // Connect the last and first points to close the polygon
     if (points.length > 2) {
       if (points.last != null && points.first != null) {
-        canvas.drawLine(points.last!, points.first!, linePaint);
+        canvas.drawLine(points.last!, points.first!, paint);
       }
     }
   }
